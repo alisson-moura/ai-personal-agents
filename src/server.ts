a3591431e537b13ae00f0b213c7fdc1a7ab8c55d
Alisson Matheus de Oliveira Moura <alisson.mo.moura@outlook.com.br>
2025-07-08 16:02:25 +0000

    build: add fastify and zod

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,24 @@
-// biome-ignore lint/suspicious/noConsole: dev only
-console.log('hello world!!');
+import cors from '@fastify/cors';
+import Fastify from 'fastify';
+import type { ZodTypeProvider } from 'fastify-type-provider-zod';
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod';
+
+const fastify = Fastify({ logger: true }).withTypeProvider<ZodTypeProvider>();
+fastify.register(cors, { origin: '*' });
+
+fastify.setValidatorCompiler(validatorCompiler);
+fastify.setSerializerCompiler(serializerCompiler);
+
+fastify.get('/', function handler() {
+  return { hello: 'world' };
+});
+
+try {
+  await fastify.listen({ port: 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
