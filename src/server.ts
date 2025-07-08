b7cb06a7d4fe6c6d2a040014c459654cd1a6ca46
Alisson Matheus de Oliveira Moura <alisson.mo.moura@outlook.com.br>
2025-07-08 16:12:40 +0000

    feat: add env validation

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import {
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod';
+import { env } from './env.ts';
 
 const fastify = Fastify({ logger: true }).withTypeProvider<ZodTypeProvider>();
 fastify.register(cors, { origin: '*' });
@@ -17,7 +18,7 @@ fastify.get('/', function handler() {
 });
 
 try {
-  await fastify.listen({ port: 3000 });
+  await fastify.listen({ port: env.HTTP_PORT });
 } catch (err) {
   fastify.log.error(err);
   process.exit(1);
